226527b0b64f6b48b644b98cb04537f886072dc0
Tanay Gaur <gaurtanay6@gmail.com>
2023-09-23 13:40:27 +0530

    added comments and ss for no timout

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ def get_numbers():
     for url in all_the_urls:
         try:
             start_time = time.time()
-            res = requests.get(url, timeout=0.5)
+            res = requests.get(url, timeout=0.5) # timeout is 0.5 seconds. We're unable to retrieve all three lists bc of the timeout limit
             if res.status_code == 200:
                 data = res.json()
                 numbers = data.get('numbers', [])
                 all_numbers_list.extend(numbers)
                 print(f"checking {url}: {numbers}")  
-            elif res.status_code == 408:   
+            elif res.status_code == 408:     # 408 is timeout
                 continue
         except Exception as e:
             print(f"Error  {url}: {str(e)}")  
             continue
 
-    merged_numbers = sorted(list(set(all_numbers_list)))
+    merged_numbers = sorted(list(set(all_numbers_list)))  # set because we want unique numbers only
     return jsonify({"numbers": merged_numbers})
 
 if __name__ == '__main__':
